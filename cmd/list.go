825916daf1ca0a573c532f3c2185fb26d523c9d0
Aaron Lee <alee@aaronosaur.us>
2019-01-04 13:22:05 -0600

    List latest tweets
    Add a new command to list the latest tweets. This should probably be
    the default root action.

---
+++ cmd/list.go
@@ -0,0 +1,66 @@
+// Copyright © 2019 Aaron lee <alee@aaronosaur.us>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"net/url"
+	"time"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/spf13/cobra"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "A list of tweets",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		api := anaconda.NewTwitterApiWithCredentials(
+			"22762491-RGeBuEmKvEagFKBrdZVliYuHkS04GPD5fA3r0HfDI",
+			"kJClOXB4vzdrapA1UuGOwPTv8rMCEzz1qXeEOqs0ZLbCH",
+			"yGl5dlBsdDxz3viOPQC4vscBo",
+			"VvKN7yq5MJU1Ulc481XLNL4zsscQthqn9xngeJ3gMlDP1ogK5c",
+		)
+
+		v := url.Values{}
+		v.Set("count", "100")
+		timeline, err := api.GetHomeTimeline(v)
+		if err != nil {
+			return err
+		}
+
+		for _, t := range timeline {
+			if t.User.FollowersCount > 10000 {
+				continue
+			}
+			created, err := t.CreatedAtTime()
+			if err != nil {
+				fmt.Println(" *** ", err.Error())
+				continue
+			}
+			ago := time.Now().Sub(created)
+			if ago.Minutes() > 30 {
+				continue
+			}
+			fmt.Printf("== %v  %v (%v)\n  %v \n", t.User.Name, ago, t.User.FollowersCount, t.Text)
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
